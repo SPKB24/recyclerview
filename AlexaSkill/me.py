2ee608bad4476094cfe2cc234947491bbb0fb26b
davemachado <dave@davemachado.com>
2017-11-05 09:18:37 -0500

    remove silly file

--- AlexaSkill/me.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-import json
-
-url = "http://wecyclr.net/text_to_material?text=plane"
-#url = "http://bartjsonapi.elasticbeanstalk.com/api/status"
-q = urllib2.Request(url)
-q.add_header('User-Agent', 'Mozilla/5.0')
-r = urllib2.urlopen(q).read()
-print(r)
