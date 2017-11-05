18ad0dee6019fcdb0d66b3cf49efd437772873a6
Ubuntu <ubuntu@ip-172-31-1-0.ec2.internal>
2017-11-05 08:36:08 -0500

    Add CORS headers

--- Server/main.go
+++ Server/main.go
@@ -13,14 +13,23 @@ type Request struct {
 func main() {
 	route := gin.Default()
 	route.GET("/text_to_material", textToMaterial)
+	route.GET("/ping", pong)
 	route.Run(":8080")
 }
 
+func pong(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "message": "pong",
+    })
+}
+
 func textToMaterial(c *gin.Context) {
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 	var req Request
 	if c.BindQuery(&req) == nil {
 		log.Println("====== Bind By Query String ======")
-		log.Println(req.Text)
+		log.Printf("Text: %s\n", req.Text)
 	}
 	/*
 	if c.BindJSON(&req) == nil {
@@ -30,7 +39,7 @@ func textToMaterial(c *gin.Context) {
 	*/
 	out, err := exec.Command("./predict", req.Text).Output()
 	if err != nil {
-		log.Println(err.Error())
+		panic(err.Error())
 	}
 	c.String(200, string(out))
 }
